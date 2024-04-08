2d286e501fa97ab291c8d83d428c02ad17771d32
AnuradhaNanayakkara <anurdha.nanayakkara@simplitude.se>
2024-04-08 22:47:59 +0200

    mapping of the course and Search Response

--- Keystone.Application/Features/SearchCourses/Mappers/SearchCourseMapper.cs
+++ Keystone.Application/Features/SearchCourses/Mappers/SearchCourseMapper.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Keystone.Application.Features.SearchCourses.Responses;
+using Keystone.Application.Helpers;
 using Keystone.Domain.Entities;
 
 namespace Keystone.Application.Features.SearchCourses.Mappers;
@@ -8,7 +9,19 @@ public class SearchCourseMapper : Profile
 {
     public SearchCourseMapper()
     {
+      
+        
         CreateMap<Course, SearchResponse>()
-            .ReverseMap();
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Institute, opt => opt.MapFrom(src => src.Institute))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location))
+            .ForMember(dest => dest.DeliveryMethod, opt => opt.MapFrom(src => EnumHelper.GetEnumDescription(src.DeliveryMethod)))
+            .ReverseMap()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Institute, opt => opt.MapFrom(src => src.Institute))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location))
+            .ForMember(dest => dest.DeliveryMethod, opt => opt.MapFrom(src => EnumHelper.GetEnumValueFromDescription<DeliveryMethod>(src.DeliveryMethod)));
     }
 }
